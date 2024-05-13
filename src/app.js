2c3b9ecfe2b29b3b06ac519f945ca94022ab8aae
Srivardhan00 <srivardhanveeramalli@gmail.com>
2024-05-13 11:57:38 +0530

    add middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-export{app}
+//setup cors middleware
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json()) //using json parsing middleware
+
+//for reading data through urls
+app.use(express.urlencoded(
+    {
+        extended:true //for reading nested objects
+    }
+)) 
+
+//to store assets, files, folders, etc
+app.use(express.static("public"))
+
+//to access user's cookies and perform CRUD operations on cookies
+app.use(cookieParser())
+export { app };
