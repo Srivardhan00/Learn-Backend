ee0c57656be88b40a1d2c0f253ffda9bc9aa5572
Srivardhan00 <srivardhanveeramalli@gmail.com>
2024-05-13 12:28:39 +0530

    custom response class

--- src/Utils/ApiError.js
+++ src/Utils/ApiError.js
@@ -1,16 +1,12 @@
-class ApiError extends Error{
-    constructor(
-        statusCode, 
-        message='Something went Wrong',
-        errors=[]
-    ){
-        super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.success = false
-        this.errors=errors
-    }
+class ApiError extends Error {
+  constructor(statusCode, message = "Something went Wrong", errors = []) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false; //as its error, its false
+    this.errors = errors;
+  }
 }
 
-export {ApiError}
+export { ApiError };
