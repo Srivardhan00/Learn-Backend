7e2da3de58e641492b543603d6285b23fdced63c
Srivardhan00 <srivardhanveeramalli@gmail.com>
2024-05-13 13:04:15 +0530

    Add AggregatePaginate package

--- src/Models/video.model.js
+++ src/Models/video.model.js
@@ -1,43 +1,47 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
-    {
-        videoFile:{
-            type:String, //cloud
-            required:true
-        },
-        title:{
-            type:String,
-            required:true            
-        },
-        description:{
-            type:String,
-            required:true            
-        },
-        thumbnail:{
-            type:String, //cloud
-            required:true            
-        },
-        duration:{
-            type:Number,
-            required:true,
-        },
-        views:{
-            type:Number,
-            default:0
-        },
-        isPublished:{ //pivate or public
-            type:Boolean,
-            default:true
-        },
-        owner:{
-            type:Schema.Types.ObjectId,
-            ref:"User"
-        }
+  {
+    videoFile: {
+      type: String, //cloud
+      required: true,
     },
-    {
-        timestamps:true
-    }
-)
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloud
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      //pivate or public
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Video  = mongoose.model("Video", videoSchema)
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
